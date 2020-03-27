ca26e47af762fab42650ac1e08e2fa0e85e18dd7
kgaurav123 <kumar123gaueav.kg@gmail,com>
2020-03-27 18:58:31 +0530

    changes

--- Mysite/Portfolio/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from.import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('about/',views.index,name='about'),
-]
--- Mysite/Portfolio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request,'index.html')
-def about(request):
-    return render(request,'about.html')
